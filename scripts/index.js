60a473b3de79773305655d9e16336a9455518e03
lala <spluyha@yandex.ru>
2025-04-17 14:50:27 +0300

    version1

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,35 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
 
 // @todo: DOM узлы
+const places = document.querySelector(".places");
+const placesList = places.querySelector(".places__list");
 
 // @todo: Функция создания карточки
+function createCard(cardInfo, funcDelete) {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardTitle = cardElement.querySelector(".card__title");
+  const deleteButton = cardElement.querySelector(".card__delete-button");
 
+  cardImage.src = cardInfo.link;
+  cardImage.alt = cardInfo.name;
+  cardTitle.textContent = cardInfo.name;
+  deleteButton.addEventListener("click", funcDelete);
+
+  return cardElement;
+}
 // @todo: Функция удаления карточки
+function deleteCard(event) {
+  const eventTarget = event.target;
+  const listItem = eventTarget.closest(".places__item");
 
+  listItem.remove();
+}
 // @todo: Вывести карточки на страницу
+
+initialCards.forEach((item) => {
+  let newCard = createCard(item, deleteCard);
+
+  placesList.append(newCard);
+});
